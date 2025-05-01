800fe1a76ed55858654041c9fab969f26c32d449
Saniya Gautam <s616271@gmail.com>
2025-05-01 20:37:13 +0530

    time and date for review

--- models/review.js
+++ models/review.js
@@ -1,23 +1,47 @@
+// const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
+
+// const reviewSchema = new Schema({
+//     comment:{
+//         type:String,
+//     },
+//     rating:{
+//         type:Number,
+//         min:1,
+//         max:5,
+//     },
+//     createdAt:{
+//         type:Date,
+//         default: Date.now(),
+//     },
+//     author:{
+//         type:Schema.Types.ObjectId,//stores user's id 
+//         ref:"User",
+//     }
+// });
+
+// module.exports=mongoose.model("Review",reviewSchema);
+
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const reviewSchema = new Schema({
-    comment:{
-        type:String,
+    comment: {
+        type: String,
     },
-    rating:{
-        type:Number,
-        min:1,
-        max:5,
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
     },
-    createdAt:{
-        type:Date,
-        default: Date.now(),
+    createdAt: {
+        type: Date,
+        default: Date.now,  // Corrected to use function reference
     },
-    author:{
-        type:Schema.Types.ObjectId,//stores user's id 
-        ref:"User",
+    author: {
+        type: Schema.Types.ObjectId,  // stores user's id
+        ref: "User",
     }
 });
 
-module.exports=mongoose.model("Review",reviewSchema);
+module.exports = mongoose.model("Review", reviewSchema);
